781731eef106734c9d3cc1601d6399bd6136aa60
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-08-11 20:59:00 +0300

    Add 7e

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - output alphabet reverse and lowercase
+ * main - ourput alphabet reverse and lowercase
  * Return: return 0 and exit the program
  */
 int main(void)
