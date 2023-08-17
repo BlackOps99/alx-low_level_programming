b82a494319c4d76fe4ca7554429b1d68c5011a94
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-08-17 19:49:04 +0300

    Add 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - Print
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+int m;
+for (m = 0; m <= 9; m++)
+{
+_putchar('0' + m);
+}
+_putchar('\n');
+}
