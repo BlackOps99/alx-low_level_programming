4efce1a288b3c826127463e33c6e39cfc6cf3d31
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-08-21 18:05:06 +0300

    Test 4

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,19 +7,26 @@
  */
 int main(void)
 {
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
 	srand(time(NULL));
 
-	int i = 0;
-	char password[12];
-
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 100; i++)
 	{
-		password[i] = (char)rand() % 94 + 33;
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}	
 	}
-
-	password[11] = '\0';
-
-	printf("%s\n", password);
 
 	return (0);
 }
