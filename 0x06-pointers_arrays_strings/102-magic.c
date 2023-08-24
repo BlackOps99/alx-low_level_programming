95a1b3fa6f451000dc8e7613bfe80ae5a0f9b0d9
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-08-24 19:12:03 +0300

    Add 102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,23 +6,21 @@
  */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
-
-	a[2] = 1024;
-	p = &n;
-
-	/*
-	 * Write your line of code here...
-	 * The code below uses a pointer arithmetic trick to modify a[2] indirectly through p.
-	 * It subtracts the difference between the memory location of a[2] and n from the value at p.
-	 * Since a[2] is 1024 and n is uninitialized, the difference is 1024 - random_value,
-	 * and subtracting that from *p changes the value stored in a[2] to 98.
-	 */
-	*(p + 5) = 98;
-
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+int n;
+int a[5];
+int *p;
+a[2] = 1024;
+p = &n;
+/*
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
