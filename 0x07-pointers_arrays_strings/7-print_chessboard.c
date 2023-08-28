9b0ef86efcf3de6626c1b923075cdff9ea2c0c0c
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-08-28 15:10:10 +0300

    Add 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - method that prints the chessboard.
+ * @a: type char str
+ * Return: always 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col; 
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
+}
