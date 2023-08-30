d393620c9369cb7325d8319b53a99bedd5ea5814
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-08-30 19:46:22 +0300

    Add 101 Edit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,44 +3,6 @@
 #include <stdbool.h>
 #include <string.h>
 /**
- * wildcmp_recursive - compares two strings
- * @s1: type char pointer
- * @s2: type char pointer
- * @i: type int
- * @j: type int
- * Return: 1 or 0
- */
-bool wildcmp_recursive(char *s1, char *s2, int i, int j)
-{
-	int k;
-	int len;
-
-	if (s1[i] == '\0' && s2[j] == '\0')
-	{
-		return (true);
-	}
-
-	if (s2[j] == '*')
-	{
-		len = strlen(s1);
-
-		for (k = i; k <= len; k++)
-		{
-			if (wildcmp_recursive(s1, s2, k, j + 1))
-			{
-				return (true);
-			}
-		}
-	}
-
-	if (s1[i] == s2[j] || s2[j] == '?')
-	{
-		return (wildcmp_recursive(s1, s2, i + 1, j + 1));
-	}
-
-	return (false);
-}
-/**
  * wildcmp - compares two strings 1 if strings can considered identical else 0
  * @s1: type char pointer
  * @s2: type char pointer
@@ -48,5 +10,13 @@ bool wildcmp_recursive(char *s1, char *s2, int i, int j)
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (wildcmp_recursive(s1, s2, 0, 0) ? 1 : 0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*' && *(s2 + 1) != '\0')
+		return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }
