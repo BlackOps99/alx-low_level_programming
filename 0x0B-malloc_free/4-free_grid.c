7b7185682f65ef810833ff6269169a5ea12ebb7f
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-04 18:08:49 +0300

    Add 4 fix betty

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * alloc_grid - function that frees a 2 dimensional grid
+ * free_grid - function that frees a 2 dimensional grid
  * @grid: int pointer type
  * @height: int type
  * Return: Null if 0 or negative
