68e15515f8cda49c2bbe2878bd36ce896c9d5e54
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-07 17:09:42 +0300

    Add 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	unsigned int i;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		return malloc(new_size);
+	}
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	if (new_size < old_size)
+	{
+		old_size = new_size;
+	}
+
+	for (i = 0; i < old_size; i++)
+	{
+		((char *)new_ptr)[i] = ((char *)ptr)[i];
+	}
+
+	free(ptr);
+
+	return (new_ptr);
+}
