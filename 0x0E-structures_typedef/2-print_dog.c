e831de42d58e3ae722fd38182076e88a92787c7a
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-12 00:03:37 +0300

    Add 2 fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 	}
 
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %.6f\n", (d->age >= 0) ? d->age : 0);
+	printf("Age: %s\n", (d->age > 0) ? printf("%.6f", d->age) : "(nil)");
 	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
