5a3633cc53091434174315b14086a928179082af
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-11 23:30:47 +0300

    Add readme and dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - The Dog main struct.
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner for that dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
