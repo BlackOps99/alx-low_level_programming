de2320cdd42b516d2f7cf97064ba9d9aea72b54c
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-12 00:28:31 +0300

    Add 4 fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -74,6 +74,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	createdog->name = _strncpy(createdog->name, name, _strlen(name));
 	createdog->age = age;
-	createdog->owner = _strncpy(createdog->owner, owner, _strlen(name));
+	createdog->owner = _strncpy(createdog->owner, owner, _strlen(owner));
 	return (createdog);
 }
