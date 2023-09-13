c59a639f55f3131c0501e7fa45ccc2b675d3a3f2
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-13 18:05:41 +0300

    Fix 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,10 +18,8 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op)
 	{
-		if (ops[i].op[0] == s[0] && s[1] == '\0')
-		{
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
-		}
 
 		++i;
 	}
