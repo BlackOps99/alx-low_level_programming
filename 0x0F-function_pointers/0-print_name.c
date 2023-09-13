c76e76957f2389f490e0593e8af3ef4bd59b6875
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-09-13 16:40:26 +0300

    Add 0, readme and header

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: type char string pointer
+ * @f: type pointer func and char as arg
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
