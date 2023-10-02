a9510ebe28e82a2c75cfabab4383f2669c05224c
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-10-02 15:58:04 +0300

    Add 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * _strlen - parameter s as pointer char
+ * @s: char pointer
+ * Return: len of char
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+/**
+ * create_file - function that creates a file.
+ * @filename: The file name that will be create
+ * @text_content: The number of letters that will write in the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, _strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
