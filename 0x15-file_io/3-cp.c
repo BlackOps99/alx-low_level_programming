5aebded9eceb915bcccaf01ba6ea53d9bfbf9417
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-10-02 16:46:56 +0300

    Fix 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,63 +6,65 @@
 #define BUFFER_SIZE 1024
 
 /**
- * exit_with_error - function handle exit codes and messages
- * @exit_code: The Status Code of exit
- * @error_message: The Messagw That Will Show After Exit
- * Return: No Thing
+ * error_file - checks if files can be opened.
+ * @fd_source: file_from.
+ * @fd_dest: file_to.
+ * @argv: arguments vector.
+ * Return: No Thing.
  */
-void exit_with_error(int exit_code, const char *error_message)
+void error_file(int fd_source, int fd_dest, char *argv[])
 {
-	dprintf(STDERR_FILENO, "%s\n", error_message);
-	exit(exit_code);
+	if (fd_source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (fd_dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
 /**
  * copy_file - function copy from and to files
  * @file_from: the file name that will copy from
  * @file_to: the file name that will copy to
- * Return: 1 on success, -1 on failure
+ * @argv: files names
+ * Return: No Thing.
  */
-int copy_file(const char *file_from, const char *file_to)
+void copy_file(const char *file_from, const char *file_to, char *argv[])
 {
 	int fd_source, fd_dest;
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read, bytes_written;
 
 	fd_source = open(file_from, O_RDONLY);
-
-	if (fd_source == -1)
-		return (-1);
-
 	fd_dest = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 
-	if (fd_dest == -1)
-	{
-		close(fd_source);
-		return (-1);
-	}
+	error_file(fd_source, fd_dest, argv);
 
 	while ((bytes_read = read(fd_source, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_dest, buffer, bytes_read);
+
 		if (bytes_written == -1)
-		{
-			close(fd_source);
-			close(fd_dest);
-			return (-1);
-		}
+			error_file(0, -1, argv);
 	}
 
 	if (bytes_read == -1)
+		error_file(-1, 0, argv);
+
+	if (close(fd_source) == -1)
 	{
-		close(fd_source);
-		close(fd_dest);
-		return (-1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_source);
+		exit(100);
 	}
 
-	if (close(fd_source) == -1 || close(fd_dest) == -1)
-		return (-1);
-
-	return (0);
+	if (close(fd_dest) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dest);
+		exit(100);
+	}
 }
 /**
  * main - the EndPoint
@@ -74,18 +76,14 @@ int main(int argc, char *argv[])
 {
 	const char *file_from = argv[1];
 	const char *file_to = argv[2];
-	int result;
 
 	if (argc != 3)
 	{
-		exit_with_error(97, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
 	}
 
-	result = copy_file(file_from, file_to);
-	if (result == -1)
-	{
-		exit_with_error(98, "Error: Can't read/write from/to file");
-	}
+	copy_file(file_from, file_to, argv);
 
 	return (0);
 }
